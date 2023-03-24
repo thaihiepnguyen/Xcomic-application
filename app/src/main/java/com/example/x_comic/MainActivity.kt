d7fe1341c0e6bf3aa73889d6d68d8bce9ed0cc19
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-03-25 00:40:53 +0700

    bottom navigation bar first build

--- app/src/main/java/com/example/x_comic/MainActivity.kt
+++ app/src/main/java/com/example/x_comic/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.x_comic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.x_comic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.explore -> replaceFragment(Explore())
+                R.id.library -> replaceFragment(Library())
+                R.id.writing -> replaceFragment(Writing())
+                R.id.notification -> replaceFragment(Notification())
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
