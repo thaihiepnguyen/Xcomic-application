fbd8547c3f2683fa7023419e29d15d4ceee2a25a
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-04-01 20:09:30 +0700

    filter ui for explore

--- app/src/main/java/com/example/x_comic/views/main/fragments/Explore.kt
+++ app/src/main/java/com/example/x_comic/views/main/fragments/Explore.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +13,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.x_comic.R
 import com.example.x_comic.adapters.CategoryAdapter
 import com.example.x_comic.adapters.ListAdapterSlideshow
+import com.github.aakira.expandablelayout.ExpandableLayout
+import com.github.aakira.expandablelayout.ExpandableLinearLayout
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout
 import com.google.android.flexbox.AlignContent
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
@@ -41,6 +46,10 @@ class Explore : Fragment() {
 
 
     val category_list: MutableList<String> = mutableListOf("Romance","Poetry","Science Fiction","Teen Fiction","Short Story","Mystery","Adventure","Thriller","Horror","Humor","LGBT+","Non Fiction","Fanfiction","Historical Fiction","Contemporary Lit","Diverse Lit","Fantasy","Paranormal","New Adult")
+    var btnLength: TextView? = null;
+    var btnStatus: TextView? = null;
+    var layoutExpand2 : ExpandableRelativeLayout? = null;
+    var layoutExpand: ExpandableRelativeLayout? = null;
 
     var categoryView : RecyclerView? = null;
     override fun onCreateView(
@@ -58,7 +67,120 @@ class Explore : Fragment() {
         layoutManager!!.flexDirection = FlexDirection.ROW;
         layoutManager!!.alignItems = AlignItems.FLEX_START;
 
+
         categoryView!!.layoutManager = layoutManager;
+
+        btnLength = view.findViewById(R.id.length);
+        btnStatus = view.findViewById(R.id.status);
+
+        layoutExpand = view.findViewById(R.id.expandableLayout);
+        layoutExpand2 = view.findViewById(R.id.expandableLayout2);
+        layoutExpand2!!.collapse();
+
+        btnStatus!!.setOnClickListener{
+            layoutExpand2!!.toggle();
+            if (layoutExpand2!!.isExpanded){
+                btnStatus!!.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.chevrondown,0)
+            }
+            else {
+                btnStatus!!.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.chevronup,0)
+
+            }
+        }
+
+        layoutExpand!!.collapse();
+        btnLength!!.setOnClickListener{
+
+            layoutExpand!!.toggle();
+            if (layoutExpand!!.isExpanded){
+                btnLength!!.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.chevrondown,0)
+            }
+            else {
+                btnLength!!.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.chevronup,0)
+
+            }
+        }
+
+
+        val checkBox1: CheckBox = view.findViewById(R.id.checkBox1);
+        val checkBox2: CheckBox = view.findViewById(R.id.checkBox2);
+        val checkBox3: CheckBox = view.findViewById(R.id.checkBox3);
+        val checkBox4: CheckBox = view.findViewById(R.id.checkBox4);
+        val checkBox5: CheckBox = view.findViewById(R.id.checkBox5);
+
+        val checkBox6: CheckBox = view.findViewById(R.id.checkBox6);
+        val checkBox7: CheckBox = view.findViewById(R.id.checkBox7);
+        val checkBox8: CheckBox = view.findViewById(R.id.checkBox8);
+        val checkBox9: CheckBox = view.findViewById(R.id.checkBox9);
+        val checkBox10: CheckBox = view.findViewById(R.id.checkBox10);
+
+
+        checkBox1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox5.isChecked = false;
+            }
+        }
+
+        checkBox2.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox5.isChecked = false;
+            }
+        }
+
+        checkBox3.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox5.isChecked = false;
+            }
+        }
+
+        checkBox4.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox5.isChecked = false;
+            }
+        }
+
+        checkBox5.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox1.isChecked = false;
+                checkBox2.isChecked = false;
+                checkBox3.isChecked = false;
+                checkBox4.isChecked = false;
+            }
+        }
+
+
+        checkBox6.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox10.isChecked = false;
+            }
+        }
+
+        checkBox7.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox10.isChecked = false;
+            }
+        }
+
+        checkBox8.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox10.isChecked = false;
+            }
+        }
+
+        checkBox9.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox10.isChecked = false;
+            }
+        }
+
+        checkBox10.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBox6.isChecked = false;
+                checkBox7.isChecked = false;
+                checkBox8.isChecked = false;
+                checkBox9.isChecked = false;
+            }
+        }
 
 
         return view
